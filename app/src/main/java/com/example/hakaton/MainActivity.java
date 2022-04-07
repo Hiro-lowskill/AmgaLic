9c974c41e4151da1ed6d716022c8aa5f37c25248
soahunsem <denic.gogolev@mail.ru>
2022-04-07 16:46:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hakaton/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hakaton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://gradeleven.ru");
+    }
+}
---
+++ app/src/main/java/com/example/hakaton/splash.java
@@ -0,0 +1,29 @@
+package com.example.hakaton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    private final int SPLASH_DSPLAY_LENGHT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(splash.this,MainActivity.class);
+                splash.this.startActivity(mainIntent);
+                splash.this.finish();
+            }
+        },SPLASH_DSPLAY_LENGHT);
+
+
+
+    }
+}
