526ff620d9d753e498a1184897ec8ea8d349e099
soahunsem <denic.gogolev@mail.ru>
2022-04-08 05:26:37 +0300

    commit final

--- app/src/main/java/com/example/hakaton/ReadActivity.java
+++ app/src/main/java/com/example/hakaton/ReadActivity.java
@@ -2,12 +2,9 @@ package com.example.hakaton;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,28 +33,25 @@ public class ReadActivity extends AppCompatActivity {
         init();
         getDataFromDB();
     }
-
-    private void init() {
+    private void init()
+    {
         listView = findViewById(R.id.listView);
         listData = new ArrayList<>();
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         listView.setAdapter(adapter);
         mDataBase = FirebaseDatabase.getInstance().getReference(USER_KEY);
     }
-
-    private void getDataFromDB() {
+    private void getDataFromDB()
+    {
         ValueEventListener vListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot datasnapshot) {
-                if (listData.size() > 0) listData.clear();
-                for (DataSnapshot ds : datasnapshot.getChildren()) {
+                if (listData.size() > 0)listData.clear();
+                for(DataSnapshot ds : datasnapshot.getChildren()) {
                     contactformmessages user = ds.getValue(contactformmessages.class);
                     assert user != null;
                     listData.add(user.name);
-
-
                 }
-
                 adapter.notifyDataSetChanged();
 
             }
@@ -66,16 +60,12 @@ public class ReadActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseerror) {
 
             }
-
         };
         mDataBase.addValueEventListener(vListener);
     }
-
     public void btn(View view){
         Intent i = new Intent(this,MainActivity.class);
         startActivity(i);
         finish();
     }
-
-
 }
